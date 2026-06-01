defdee25deb50058b1010cb746861b8fd354ffc4
Osura Viduranga <hackdarkmi7@gmail.com>
2026-06-01 13:14:42 +0530

    Devportal prod search fix

--- portals/devportal/src/main/webapp/source/src/app/components/Base/Header/Search/SearchUtils.jsx
+++ portals/devportal/src/main/webapp/source/src/app/components/Base/Header/Search/SearchUtils.jsx
@@ -175,6 +175,7 @@ function renderInput(inputProps) {
 function getPath(suggestion) {
     switch (suggestion.type) {
         case 'API':
+        case 'APIPRODUCT':
             return `/apis/${suggestion.id}/overview`;
         case 'DEFINITION':
             return `/apis/${suggestion.apiUUID}/overview`;
@@ -195,9 +196,10 @@ function renderSuggestion(suggestion, { query, isHighlighted }) {
     const parts = parse(suggestion.name, matches);
     const path = getPath(suggestion);
     // TODO: Style the version ( and apiName if docs) apearing in the menu item
-    const suffix = suggestion.type === 'API' ? suggestion.version : (suggestion.apiName + ' ' + suggestion.apiVersion);
+    const suffix = (suggestion.type === 'API' || suggestion.type === 'APIPRODUCT') ? suggestion.version
+        : (suggestion.apiName + ' ' + suggestion.apiVersion);
     const getIcon = (type) => {
-        if (type === 'API') {
+        if (type === 'API' || type === 'APIPRODUCT') {
             return <APIsIcon />;
         } else if (type === 'DEFINITION') {
             return <CodeIcon />;
